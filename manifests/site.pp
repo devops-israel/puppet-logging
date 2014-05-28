0e790d6c00c40d4997e928ea80d9287eaf91520b
Evgeny Zislis <evgeny.zislis@gmail.com>
2014-05-28 04:01:26 +0300

    consolidate kibana to the same node as elasticsearch

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,12 +2,9 @@ Exec { path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
 
 node elasticsearch {
   include logging::elasticsearch
+  include logging::kibana
 }
 
 node logstash {
   include logging::logstash
-}
-
-node kibana {
-  include logging::kibana
 }
