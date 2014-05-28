2a81817bfcb2a85f635e0b4fca85c4dc547d6d84
Evgeny Zislis <evgeny.zislis@gmail.com>
2014-05-28 06:47:56 +0300

    use logstash module configfile resource

--- site-modules/logging/manifests/logstash.pp
+++ site-modules/logging/manifests/logstash.pp
@@ -1,15 +1,17 @@
 class logging::logstash {
+
   class { "::logstash": }
-  file { "/etc/logstash/conf.d/input-log4j.conf":
-    source => "puppet:///modules/logging/logstash/input-log4j.conf",
-    notify => Service["logstash"],
+
+  logstash::configfile {
+    "input-log4j":
+      source => "puppet:///modules/logging/logstash/input-log4j.conf",
+      ;
+    "input-syslog":
+      source => "puppet:///modules/logging/logstash/input-syslog.conf",
+      ;
+    "output-elasticsearch":
+      source => "puppet:///modules/logging/logstash/output-elasticsearch.conf",
+      ;
   }
-  file { "/etc/logstash/conf.d/input-syslog.conf":
-    source => "puppet:///modules/logging/logstash/input-syslog.conf",
-    notify => Service["logstash"],
-  }
-  file { "/etc/logstash/conf.d/output-elasticsearch.conf":
-    source => "puppet:///modules/logging/logstash/output-elasticsearch.conf",
-    notify => Service["logstash"],
-  }
+
 }
