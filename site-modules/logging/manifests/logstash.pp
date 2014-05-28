619446bea8042ce2f4a1f0a013ac874ba4868b81
Evgeny Zislis <evgeny.zislis@gmail.com>
2014-05-28 06:43:58 +0300

    add logstash configuration

--- site-modules/logging/manifests/logstash.pp
+++ site-modules/logging/manifests/logstash.pp
@@ -1,3 +1,15 @@
 class logging::logstash {
   class { "::logstash": }
+  file { "/etc/logstash/conf.d/input-log4j.conf":
+    source => "puppet:///modules/logging/logstash/input-log4j.conf",
+    notify => Service["logstash"],
+  }
+  file { "/etc/logstash/conf.d/input-syslog.conf":
+    source => "puppet:///modules/logging/logstash/input-syslog.conf",
+    notify => Service["logstash"],
+  }
+  file { "/etc/logstash/conf.d/output-elasticsearch.conf":
+    source => "puppet:///modules/logging/logstash/output-elasticsearch.conf",
+    notify => Service["logstash"],
+  }
 }
